62ffc869d0f8446759bcdf3d62c1720e9d997cfa
Jin Sasaki <aysm47@gmail.com>
2016-01-11 19:51:05 +0900

    Update sample app

--- TableViewSwipeSample/ViewController.swift
+++ TableViewSwipeSample/ViewController.swift
@@ -8,27 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var tableView: UITableView!
+    
+    var swipeEnabled = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tableView.dataSource = self
-        self.tableView.allowsSelection = false
-        
-        
-        self.tableView.swipeEnabled(true, delegate: self)
+        self.tableView.delegate = self
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "cell")
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
         cell.textLabel?.text = "swipe \(indexPath.row)"
         return cell
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 50
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 100
+    }
+    
+    func tableView(tableView: UITableView, willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath? {
+        return indexPath
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        self.swipeEnabled = !self.swipeEnabled
+        self.tableView.swipeEnabled(self.swipeEnabled, delegate: self)
     }
 }
 
